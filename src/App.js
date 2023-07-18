088a96c29d0aa747879bf6e88e9df43e1dad4194
Baleria Reyes <breyes815@gmail.com>
2023-07-18 15:21:18 -0700

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+// import Home from './components/Home';
+import About from './components/AboutMe/AboutMe';
 import AboutMe from './components/AboutMe/AboutMe';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Header
-      </header>
-      <AboutMe />
-      Other Components
-    </div>
-  )
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                {/* <Route path="/" element={<Home />} /> */}
+                <Route path="/about" element={<AboutMe />} />
+                {/* <Route path="/projects" element={<Projects />} /> */}
+                {/* <Route path="/contact" element={<Contact />} /> */}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
