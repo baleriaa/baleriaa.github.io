8b47d7051e837490770f9474b48b72e7c5b2d824
Baleria Reyes <breyes815@gmail.com>
2023-07-18 13:43:21 -0700

    Add props for PersonalInfo and edit structure

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ProfileImage from './ProfileImage';
+import PersonalInfo from './PersonalInfo';
 import Bio from './Bio';
 import SkillsList from './SkillsList';
 import ResumeButton from './ResumeButton';
 import SocialLinks from './SocialLinks';
 
+const info = [
+  { label: "Name", value: "Baleria Reyes" },
+  { label: "Location", value: "Bend, Oregon" },
+  { label: "Education", value: "Oregon State University computer science student" },
+  { label: "Languages", value: "Fluent in English and Spanish" },
+];
+
 const AboutMe = () => {
     return (
         <section id="about-me">
-            <ProfileImage />
-            <PersonalInfo />
-            <Bio />
-            <SkillsList />
-            <ResumeButton />
-            <SocialLinks />
+            <PersonalInfo info={info} />
+            {/* other components... */}
         </section>
     );
 }
